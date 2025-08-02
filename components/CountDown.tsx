b4064ea7290abd97b972fee8e8999611f143ee2f
hryhorii_shkarta <grishkoshkarta1@gmail.com>
2025-08-02 14:22:37 +0300

    add date

--- components/CountDown.tsx
+++ components/CountDown.tsx
@@ -1,44 +1,46 @@
 'use client'
 
-// import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Countdown() {
-	// const targetDate = new Date('2025-05-10T00:00:00') // Старт 10 квітня
-	// const [timeLeft, setTimeLeft] = useState({
-	// 	days: 0,
-	// 	hours: 0,
-	// 	minutes: 0,
-	// })
+	const targetDate = new Date('2025-09-08T00:00:00') // Старт 10 квітня
+	const [timeLeft, setTimeLeft] = useState({
+		days: 0,
+		hours: 0,
+		minutes: 0,
+	})
 
-	// 	useEffect(() => {
-	// 		const interval = setInterval(() => {
-	// 			const now = new Date()
-	// 			const difference = targetDate.getTime() - now.getTime()
-	//
-	// 			if (difference > 0) {
-	// 				const days = Math.floor(difference / (1000 * 60 * 60 * 24))
-	// 				const hours = Math.floor((difference / (1000 * 60 * 60)) % 24)
-	// 				const minutes = Math.floor((difference / (1000 * 60)) % 60)
-	// 				setTimeLeft({ days, hours, minutes })
-	// 			} else {
-	// 				clearInterval(interval)
-	// 			}
-	// 		}, 1000)
-	//
-	// 		return () => clearInterval(interval)
-	// 	}, [])
+	useEffect(() => {
+		const interval = setInterval(() => {
+			const now = new Date()
+			const difference = targetDate.getTime() - now.getTime()
+
+			if (difference > 0) {
+				const days = Math.floor(difference / (1000 * 60 * 60 * 24))
+				const hours = Math.floor((difference / (1000 * 60 * 60)) % 24)
+				const minutes = Math.floor((difference / (1000 * 60)) % 60)
+				setTimeLeft({ days, hours, minutes })
+			} else {
+				clearInterval(interval)
+			}
+		}, 1000)
+
+		return () => clearInterval(interval)
+	}, [])
 
 	return (
 		<section className='py-20 px-4 sm:px-6 bg-black text-white'>
 			<div className='max-w-4xl mx-auto text-center'>
-				{/* <h2 className='text-3xl md:text-4xl font-bold mb-8'>
+				<h2 className='text-3xl md:text-4xl font-bold mb-8'>
 					Старт{' '}
 					<span className='bg-gradient-to-r from-red-600 to-purple-600 bg-clip-text text-transparent'>
-						10 травня
+						8 вересня
 					</span>
 				</h2>
 
-				<p className='mb-4 text-xl'>Навчання щосуботи о 13:00 за Києвом</p>
+				<p className='mb-4 text-xl'>
+					Навчання понеділок, четвер о 11:00 за Києвом
+				</p>
 
 				<h2 className='text-2xl md:text-4xl font-bold mb-8'>Залишилось</h2>
 
@@ -51,13 +53,16 @@ export default function Countdown() {
 							</span>
 						</div>
 					</div>
-
-				</div> */}
-				<h2 className='text-3xl md:text-4xl font-bold mb-8'>
-					{/* НАБІР ДО ГРУПИ ЗАКРИТИЙ */}
-					Набір у групу на вересень відкрито!
-				</h2>
-				<p>Слідкуй за актуальною інформацією у соціальних мережах</p>
+				</div>
+				{/* <h2 className='text-3xl md:text-4xl font-bold mb-8'> */}
+				{/* НАБІР ДО ГРУПИ ЗАКРИТИЙ */}
+				{/* Набір у групу на вересень відкрито! */}
+				{/* </h2> */}
+				{/* <p>Слідкуй за актуальною інформацією у соціальних мережах</p> */}
+				{/* <p>Графік занять:</p> */}
+				{/* <p>
+					Понеділок, четвер: 11:00 - 13:00 (тривалість заняття від 1 до 2 годин)
+				</p> */}
 			</div>
 		</section>
 	)
