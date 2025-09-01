b42837fc97531505fb15774196d34b322144b0c4
hryhorii_shkarta <grishkoshkarta1@gmail.com>
2025-09-01 09:01:21 +0300

    fix count

--- components/CountDown.tsx
+++ components/CountDown.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
 
 export default function Countdown() {
 	const targetDate = new Date('2025-09-08T00:00:00') // Старт 10 квітня
@@ -31,34 +31,34 @@ export default function Countdown() {
 	return (
 		<section className='py-20 px-4 sm:px-6 bg-black text-white'>
 			<div className='max-w-4xl mx-auto text-center'>
-				<h2 className='text-3xl md:text-4xl font-bold mb-8'>
-					Старт{' '}
-					<span className='bg-gradient-to-r from-red-600 to-purple-600 bg-clip-text text-transparent'>
-						8 вересня
-					</span>
-				</h2>
-
-				<p className='mb-4 text-xl'>
-					Навчання понеділок, четвер о 11:00 за Києвом
-				</p>
-
-				<h2 className='text-2xl md:text-4xl font-bold mb-8'>Залишилось</h2>
-
-				<div className='flex justify-center gap-6 text-2xl font-semibold'>
-					<div className='p-[2px] rounded-2xl bg-gradient-to-r from-red-600 to-purple-600 shadow-lg'>
-						<div className='flex flex-col items-center justify-center px-8 py-6 bg-black rounded-2xl backdrop-blur-md'>
-							<span className='text-6xl font-extrabold'>{timeLeft.days}</span>
-							<span className='mt-2 text-lg uppercase tracking-widest'>
-								Днів
-							</span>
-						</div>
-					</div>
-				</div>
-				{/* <h2 className='text-3xl md:text-4xl font-bold mb-8'> */}
-				{/* НАБІР ДО ГРУПИ ЗАКРИТИЙ */}
+				{/*<h2 className='text-3xl md:text-4xl font-bold mb-8'>*/}
+				{/*	Старт{' '}*/}
+				{/*	<span className='bg-gradient-to-r from-red-600 to-purple-600 bg-clip-text text-transparent'>*/}
+				{/*		8 вересня*/}
+				{/*	</span>*/}
+				{/*</h2>*/}
+    
+				{/*<p className='mb-4 text-xl'>*/}
+				{/*	Навчання понеділок, четвер о 11:00 за Києвом*/}
+				{/*</p>*/}
+    
+				{/*<h2 className='text-2xl md:text-4xl font-bold mb-8'>Залишилось</h2>*/}
+    
+				{/*<div className='flex justify-center gap-6 text-2xl font-semibold'>*/}
+				{/*	<div className='p-[2px] rounded-2xl bg-gradient-to-r from-red-600 to-purple-600 shadow-lg'>*/}
+				{/*		<div className='flex flex-col items-center justify-center px-8 py-6 bg-black rounded-2xl backdrop-blur-md'>*/}
+				{/*			<span className='text-6xl font-extrabold'>{timeLeft.days}</span>*/}
+				{/*			<span className='mt-2 text-lg uppercase tracking-widest'>*/}
+				{/*				Днів*/}
+				{/*			</span>*/}
+				{/*		</div>*/}
+				{/*	</div>*/}
+				{/*</div>*/}
+				 <h2 className='text-3xl md:text-4xl font-bold mb-8'>
+				 НАБІР ДО ГРУПИ ЗАКРИТИЙ
 				{/* Набір у групу на вересень відкрито! */}
-				{/* </h2> */}
-				{/* <p>Слідкуй за актуальною інформацією у соціальних мережах</p> */}
+				 </h2>
+				 <p>Слідкуй за актуальною інформацією у соціальних мережах</p>
 				{/* <p>Графік занять:</p> */}
 				{/* <p>
 					Понеділок, четвер: 11:00 - 13:00 (тривалість заняття від 1 до 2 годин)
